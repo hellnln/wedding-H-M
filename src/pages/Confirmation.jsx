0a45c388f8dbd32ca6d838990542bc1c79f56512
hellnln <helenerousseau@gmail.com>
2023-03-08 10:34:38 +0100

    css page hotel + page confirmation

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,76 +1,150 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const wait = function (duration = 1000) {
-    return new Promise((resolve) => {
-       window.setTimeout(resolve, duration) 
-    })
-    
-}
-
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, duration);
+  });
+};
 
 function Confirm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
 
-  const {register, handleSubmit, formState: {errors, isValid}} = useForm() 
+  const {
+    register: registerPresence,
+    formState: { errors: errorsPresence },
+    handleSubmit: handleSubmitPresence,
+  } = useForm({
+    mode: "onBlur",
+  });
 
 
-  const onSubmit = async data => {
-    await wait(2000)
-    console.log('toto', data)
-  }
+  const [displayPresence, setDisplayPresence] = useState(false);
+  const [displayRemerciement, setDisplayRemerciement] = useState(false);
 
   const items = [
     {
-        nom: 'Rousseau',
-        prenom: 'Anne',
-        typeInvitation: 'complet',
+      id: "1",
+      nom: "Rousseau",
+      prenom: "Anne",
+      typeInvitation: "complet",
     },
 
     {
-        nom: 'Rousseau',
-        prenom: 'Maria',
-        typeInvitation: 'cocktail',
+      id: "2",
+      nom: "Rousseau",
+      prenom: "Maria",
+      typeInvitation: "cocktail",
     },
 
     {
-        nom: 'Desse',
-        prenom: 'Mathilde',
-        typeInvitation: 'complet',
-    }
+      id: "3",
+      nom: "Desse",
+      prenom: "Mathilde",
+      typeInvitation: "complet",
+    },
+  ];
+
+  const onSubmit = async (data) => {
+    console.log("data", data);
+    // await wait(2000); // simulate query to airtable wait for api response items
+    setDisplayPresence(true)
+  };
+
+  const onSubmitPresence = async (data) => {
+    console.log("data", data);
+    // await wait(2000); // simulate query to airtable wait for api response items
+    setDisplayRemerciement(true)
+  };
 
 
-  ]
 
-    return (
-        <div> 
-            <div className='confirm'>
-                <h1 className='confirm__title'>Confirmez votre présence</h1>
-                <p className="confirm__text">Pour confirmer votre présence à notre mariage, écrivez votre nom et prénom et cliquez sur rechercher.<br/>
-                Si vous êtes bien dans la liste des invités vous pourrez confirmer votre venue !</p>
+
+  return (
+    <div>
+      <div className="confirm">
+        <h1 className="confirm__title">Confirmez votre présence</h1>
+        <p className="confirm__text">
+          Pour confirmer votre présence à notre mariage, écrivez votre nom et
+          prénom et cliquez sur rechercher.
+          <br />
+          Si vous êtes bien dans la liste des invités vous pourrez confirmer
+          votre venue !
+        </p>
+      </div>
+      <hr className="hrConfirm" />
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form__mainBox">
+          <div className="form__inputBox">
+            <div className="form__box">
+              <label className="form__title" htmlFor="prenom">
+                Prénom
+              </label>
+              <input
+                {...register("prenom", {
+                  required: "Merci de saisir votre prénom",
+                })}
+                className="confirm__input"
+                type="text"
+              />
+              {errors.prenom && <span>{errors.prenom.message}</span>}
             </div>
-            <hr className="hrConfirm" />
-            <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                <div className="form__mainBox">
-                    <div className="form__inputBox">
-                        <div className="form__box">
-                            <label className="form__title" htmlFor="prenom">Prénom</label>
-                            <input {...register('prenom', { required: "Merci de saisir votre prénom" })} className="confirm__input" type="text"/>
-                            {errors.prenom && <span>{errors.prenom.message}</span>}
-                        </div>
-                        <div className="form__box">
-                            <label className="form__title" htmlFor="nom">Nom</label>
-                            <input {...register('nom', { required: "Merci de saisir votre nom" })} className="confirm__input" type="text" /> 
-                            {errors.nom && <span>{errors.nom.message}</span>}
-                        </div>
-                    </div>
-                    
-                    <div className="form__boxBtn">
-                        <button className="form__btn" disabled={!isValid}>Rechercher</button>
-                    </div>
-                </div>
-            </form>
-            <hr className="hrConfirm" />
+            <div className="form__box">
+              <label className="form__title" htmlFor="nom">
+                Nom
+              </label>
+              <input
+                {...register("nom", {
+                  required: "Merci de saisir votre nom",
+                })}
+                className="confirm__input"
+                type="text"
+              />
+              {errors.nom && <span>{errors.nom.message}</span>}
+            </div>
+          </div>
+
+          <div className="form__boxBtn">
+            <button className="form__btn" disabled={!isValid}>
+              Rechercher
+            </button>
+          </div>
         </div>
-    )
+      </form>
+      <hr className="hrConfirm" />
+      <div>
+        {displayPresence === true && 
+          <form onSubmit={handleSubmitPresence(onSubmitPresence)}>
+        <div>
+          <input {...registerPresence("cocktail")} type="checkbox" />
+          <label htmlFor="cocktail">Je serai présent au cocktail</label>
+        </div>
+        <div>
+          <input {...registerPresence("diner")} type="checkbox" />
+          <label htmlFor="diner">Je serai présent au dîner</label>
+        </div>
+        <div>
+          <input {...registerPresence("lendemain")} type="checkbox" />
+          <label htmlFor="lendemain">Je serai présent le lendemain</label>
+        </div>
+        <div>
+          <input {...registerPresence("absent")} type="checkbox" />
+          <label htmlFor="absent">Je ne pourrai pas être présent à votre mariage</label>
+        </div>
+        <button>Envoyer</button>
+      </form>
+      }
+      {displayRemerciement === true &&
+      <div>Votre réponse a été prise en compte</div>
+      }
+      
+      </div>
+    </div>
+  );
 }
 
 export default Confirm;
--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,73 +1,88 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function Hotels() {
   return (
     <div>
       <div className="titre">
         <h1 className="titre__page">Où se loger ?</h1>
       </div>
-      <article>
-        <h2>
+      <article className="logement">
+        <h2 className="logement__titre2">
           Si vous avez besoin de vous loger sur place, trois solutions sont
           possibles :
         </h2>
-        <article>
-          <h3>Le camping</h3>
-          <ul>
-            <li>En campign-car : vous pourrez garer votre véhicule sur le parking du
+        <div className='logement__mainBox'>
+        <article className="logement__box">
+          <h3 className="logement__titre3">Le camping</h3>
+          <ul className="logement__textBox">
+            <li className="logement__text"><FontAwesomeIcon icon="caravan" className="logement__picto" />En camping-car : vous pourrez garer votre véhicule sur le parking du
             gîte</li>
-            <li>Sous la tente : vous pourrez planter votre tente sur le terrain du
+            <li className="logement__text"><FontAwesomeIcon icon="tent" className="logement__picto" />Sous la tente : vous pourrez planter votre tente sur le terrain du
             gîte</li>
           </ul>
         </article>
-        <article>
-          <h3>L'hôtel</h3>
-          <ul>
-            <li>
+        {/* <hr className='logement__ligne'></hr> */}
+        <article className="logement__box">
+          <h3 className="logement__titre3">L'hôtel</h3>
+          <ul className="logement__textBox">
+            <li className="logement__text">
               <a
+                className="logement__link"
                 href="https://reservation-hotel.logishotels.com/fo/booking/12/2228/availability?specialMode=default&begindate=08%2F07%2F2023&partid=1551&tmonth=07&tyear=2023&hotelid=2228&numnight=1&m=booking&nbpax=2&langcode=FR&enddate=09%2F07%2F2023&fyear=2023&custid=12&fmonth=07&fday=08&currency=EUR&tday=09"
                 target="_blank"
                 rel="noreferrer noopener"
-              >Hôtel Saint Louis - La Suze-Sur-Sarthe - à environ 10min du lieu de réception
+              >Hôtel Saint Louis
               </a>
+              - La Suze-Sur-Sarthe - à environ 10min du lieu de réception
             </li>
-            <li>
+            <li className="logement__text">
               <a
+                className="logement__link"
                 href="https://hotel-le-mans-cottage.brithotel.fr/chambres?action=moteur_resa&checkindate=08%2F07%2F2023&checkoutdate=09%2F07%2F2023&partid=88&nb_adt=2&curcode=EUR&desktop=&ghftotalprice=64.30&ghfusercountry=FR&googlesite=mapresults&isrobot=false&bookingSource=GoogleCPC&utm_campaign=Google_360&campaignid=19409624839&utm_source=GoogleHotel&utm_medium=cpc&utm_content=11533&nb_adt=2&isHotelCampaign=true&gclid=Cj0KCQiA0oagBhDHARIsAI-BbgclM7vMyPfxujJwswUHwskB_PLUpir0rmcu0yWqrnJWeAQ9LXVojJ4aAqHIEALw_wcB#resa"
                 target="_blank"
                 rel="noreferrer noopener"
-              >Brit Hotel - Arnage - à environ 20min du lieu de réception
+              >Brit Hotel
               </a>
+              - Arnage - à environ 20min du lieu de réception
             </li>
-            <li>
-                <a href="https://www.odalys-vacances.com/location-ville/pays-de-la-loire/le-mans/odalys-city-le-mans-centre-congres.html?idprestation=26828&idtypeprestation=1&tab=2&datedebut=2023-07-08&duree=1&esl-k=google-hotelads|ng|c111111111111|me|khotel-ads|p|t|dc|a222222222222|g333333333333&gclid=Cj0KCQiA0oagBhDHARIsAI-Bbgc0szDDzN__avyVI_TUmeAkTShUPx8JrrIZyz8_H1tsQNpSpJuTlEYaAsApEALw_wcB?wh_token=9afa262d-898e-4c8f-bcc1-5a194c7b30e5-203571-pms&utm_source=GoogleHPA&utm_medium=MapResults&utm_campaign=MetaI%2FO&utm_content=FR&utm_term="
-                 target="_blank"
-                 rel="noreferrer noopener"
-                >
-                    Odalys Appart'hôtel - Le Mans - à environ 30min du lieu de réception
+            <li className="logement__text">
+                <a 
+                className="logement__link"href="https://www.odalys-vacances.com/location-ville/pays-de-la-loire/le-mans/odalys-city-le-mans-centre-congres.html?idprestation=26828&idtypeprestation=1&tab=2&datedebut=2023-07-08&duree=1&esl-k=google-hotelads|ng|c111111111111|me|khotel-ads|p|t|dc|a222222222222|g333333333333&gclid=Cj0KCQiA0oagBhDHARIsAI-Bbgc0szDDzN__avyVI_TUmeAkTShUPx8JrrIZyz8_H1tsQNpSpJuTlEYaAsApEALw_wcB?wh_token=9afa262d-898e-4c8f-bcc1-5a194c7b30e5-203571-pms&utm_source=GoogleHPA&utm_medium=MapResults&utm_campaign=MetaI%2FO&utm_content=FR&utm_term="
+                target="_blank"
+                rel="noreferrer noopener"
+                >Odalys Appart'hôtel
                 </a>
+                - Le Mans - à environ 30min du lieu de réception
             </li>
-            <li>
-                <a href="https://www.kyriad.com/rooms/?kameleoonEnabled=false&useSynXis=false&searchType=rooms&sr=METAGHACPC&arrival=2023-07-08&departure=2023-07-09&resortCode=FRA23178&rooms[0][adult]=2&rooms[0][child]=0&utm_source=google&utm_medium=HAC&utm_campaign=FRA23178&&adType=1&utm_content=hpa_08_07_2023_1_mapresults_FRA23178_FR_desktop_selected_12234517357__2_standard&gclid=Cj0KCQiA0oagBhDHARIsAI-Bbge6Fg3SS85y_Q1UF1LLOgErEXNz3CAlB-RJw01vdhicFwn6R-osy4UaAnulEALw_wcB"
-                 target="_blank"
-                 rel="noreferrer noopener"
+            <li className="logement__text">
+                <a 
+                className="logement__link"
+                href="https://www.kyriad.com/rooms/?kameleoonEnabled=false&useSynXis=false&searchType=rooms&sr=METAGHACPC&arrival=2023-07-08&departure=2023-07-09&resortCode=FRA23178&rooms[0][adult]=2&rooms[0][child]=0&utm_source=google&utm_medium=HAC&utm_campaign=FRA23178&&adType=1&utm_content=hpa_08_07_2023_1_mapresults_FRA23178_FR_desktop_selected_12234517357__2_standard&gclid=Cj0KCQiA0oagBhDHARIsAI-Bbge6Fg3SS85y_Q1UF1LLOgErEXNz3CAlB-RJw01vdhicFwn6R-osy4UaAnulEALw_wcB"
+                target="_blank"
+                rel="noreferrer noopener"
                 >
-                    Hôtel Kyriad - La Flèche - à environ 20min du lieu de réception
+                    Hôtel Kyriad
                 </a>
+                - La Flèche - à environ 20min du lieu de réception
             </li>
           </ul>
         </article>
-        <article>
-          <h3>Louer un gîte</h3>
-          <ul>
-            <li>
+        {/* <hr className='logement__ligne'></hr> */}
+        <article className="logement__box">
+          <h3 className="logement__titre3">Louer un gîte</h3>
+          <ul className="logement__textBox">
+            <li className="logement__text">
               <a
+                className="logement__link"
                 href="https://www.airbnb.fr/s/C%C3%A9rans~Foulletourte--France/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&price_filter_input_type=0&price_filter_num_nights=1&query=C%C3%A9rans-Foulletourte%2C%20France&place_id=ChIJYXwtV6Oe4kcRA7JjOoT7lb4&date_picker_type=calendar&checkin=2023-07-08&checkout=2023-07-09&adults=2&source=structured_search_input_header&search_type=user_map_move&ne_lat=47.84928758254699&ne_lng=0.10840586264308172&sw_lat=47.80451344199594&sw_lng=0.04394701559718328&zoom=14&search_by_map=true"
                 target="_blank"
                 rel="noreferrer noopener"
               >Airbnb
               </a>
             </li>
-            <li>
+            <li className="logement__text">
               <a
+                className="logement__link"
                 href="https://www.gites-de-france.com/fr/search?destination=C%C3%A9rans-Foulletourte&towns=64080&travelers=2&seed=531da5a8&arrival=2023-07-08&departure=2023-07-09"
                 target="_blank"
                 rel="noreferrer noopener"
@@ -76,6 +91,7 @@ function Hotels() {
             </li>
           </ul>
         </article>
+        </div>
       </article>
     </div>
   );
